44b15485c3c58b073acbf6868944602cc00745c0
mf-Q <loud_flamingo@hotmail.com>
2019-12-24 00:30:55 +0900

    success_new_post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,17 +4,34 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+     @product = Product.new
   end
 
   def create
-    Product.create(product_params)
+    @product = Product.new(title: product_params[:title],
+                           genre: product_params[:genre],
+                           timing: product_params['timing(1i)'] + "/" + product_params['timing(2i)'] + "/" + product_params['timing(3i)'],
+                           place: product_params[:place],
+                           number: product_params[:number],
+                           content: product_params[:content],
+                           user_id: current_user.id
+                          )
+    @product.save
+    redirect_to("/")
   end
-
+  
 
 
   private
   def product_params
-    params.require(:product).permit(:title, :genre, :timing, :place, :number, :content, :user_id )
+    params.require(:product).permit(:title,
+                                    :genre,
+                                    :timing,
+                                    'timing(1i)',
+                                    'timing(2i)',
+                                    'timing(3i)',
+                                    :place,
+                                    :number,
+                                    :content).merge(user_id: current_user.id)
   end
 end
