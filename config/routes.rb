977a964a210815b86920ab0b365ad56555343589
mf-Q <loud_flamingo@hotmail.com>
2019-12-29 12:46:08 +0900

    add update and delete action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root  'products#index'
-  resources :products, only: [:index, :show, :edit, :new, :create, :update]
+  resources :products
   resources :users, only: [:edit, :update]
 end
